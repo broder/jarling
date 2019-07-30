61b7c1c98a8a1e3ba7ee88c42dca886a11425a22
raamSoftwire <33458055+raamSoftwire@users.noreply.github.com>
2019-07-30 13:57:51 +0100

    Revert "Add test for insufficient funds"

--- src/main/java/org/jarling/v2/models/individuals/Individual.java
+++ src/main/java/org/jarling/v2/models/individuals/Individual.java
@@ -1,7 +1,6 @@
 package org.jarling.v2.models.individuals;
 
 import lombok.Data;
-import lombok.ToString;
 
 import java.time.LocalDate;
 
@@ -9,7 +8,6 @@ import java.time.LocalDate;
  * Information about an individual account holder
  */
 @Data
-@ToString(onlyExplicitlyIncluded = true)
 public class Individual {
     private String firstName;
     private String lastName;
